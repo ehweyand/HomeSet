bbffb3b1313ee1a48883b0cbf3adbd62d447df90
Evandro Henrique <ehweyand@universo.univates.br>
2021-09-12 15:53:59 -0300

    basic authentication

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -43,14 +43,14 @@ class AuthScreen extends StatelessWidget {
                       margin: EdgeInsets.only(bottom: 20.0),
                       padding:
                           EdgeInsets.symmetric(vertical: 8.0, horizontal: 94.0),
-                      transform: Matrix4.rotationZ(-8 * pi / 180)
+                      transform: Matrix4.rotationZ(-6 * pi / 180)
                         ..translate(-10.0),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
-                        color: Colors.deepOrange.shade900,
+                        color: Theme.of(context).primaryColor,
                         boxShadow: [
                           BoxShadow(
-                            blurRadius: 8,
+                            blurRadius: 12,
                             color: Colors.black26,
                             offset: Offset(0, 2),
                           )
